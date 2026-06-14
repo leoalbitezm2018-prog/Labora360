6dcc545e08ded159af32edfe1be3262a87c9c76a
leoalbitezm2018-prog <leoalbitezm2018@gmail.com>
2026-06-14 14:09:46 -0500

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,48 +1,15 @@
-async function enviarMensaje(){
+async function enviarMensaje() {
+    const mensaje = document.getElementById("mensaje").value;
 
-const input=document.getElementById("mensaje");
-const texto=input.value.trim();
+    const respuesta = await fetch("/chat", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            mensaje: mensaje
+        })
+    });
 
-if(!texto) return;
-
-const chat=document.getElementById("chat-box");
-
-chat.innerHTML+=`
-<div class="user-message">${texto}</div>
-`;
-
-input.value="";
-chat.innerHTML += `
-<div class="bot-message" id="typing">
-🏢 Labora360 está resolviendo tu consulta...
-</div>
-`;
-
-chat.scrollTop = chat.scrollHeight;
-const respuesta=await fetch("/chat",{
-method:"POST",
-headers:{
-"Content-Type":"application/json"
-},
-body:JSON.stringify({
-mensaje:texto
-})
-});
-
-const data=await respuesta.json();
-document.getElementById("typing").remove();
-chat.innerHTML+=`
-<div class="bot-message">${data.respuesta}</div>
-`;
-
-chat.scrollTop=chat.scrollHeight;
+    const data = await respuesta.json();
 }
-
-document.getElementById("mensaje")
-.addEventListener("keypress",function(e){
-
-if(e.key==="Enter"){
-enviarMensaje();
-}
-
-});
