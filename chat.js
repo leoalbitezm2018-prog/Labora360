e7e611442a031d5d4eb13470dd42d434168184df
leoalbitezm2018-prog <leoalbitezm2018@gmail.com>
2026-06-14 14:11:30 -0500

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,15 +1,1 @@
-async function enviarMensaje() {
-    const mensaje = document.getElementById("mensaje").value;
 
-    const respuesta = await fetch("/chat", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            mensaje: mensaje
-        })
-    });
-
-    const data = await respuesta.json();
-}
